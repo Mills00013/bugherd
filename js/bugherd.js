0d33a3f5cb7d91d27f784c6f925d1e2939f79b1b
Miles McLean <Mills00013@gmail.com>
2017-02-06 16:10:24 -0600

    Applied patch https://www.drupal.org/files/issues/drupal-8-port-2104335-13.patch to prepare for D8

---
+++ js/bugherd.js
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * JS to init Bugherd with the proper api key.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.bugherd = {
+    attach: function (context, settings) {
+
+      // Get the bugherd settings so we can extract the api key.
+      var config = settings.bugherd;
+      console.log(config);
+
+      // Add bugherd embed script.
+      (function (d, t) {
+        var bh = d.createElement(t), s = d.getElementsByTagName(t)[0];
+        bh.type = 'text/javascript';
+        bh.src = '//www.bugherd.com/sidebarv2.js?apikey='+config.api_key;
+        s.parentNode.insertBefore(bh, s);
+      })(document, 'script');
+
+    }
+  };
+})(jQuery, Drupal);
